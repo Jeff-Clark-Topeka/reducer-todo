2d7f5500ac622c5087805c8e6fb5e04da59dcd3e
Jeff-Clark-Topeka <jeffclarktopeka@yahoo.com>
2020-09-16 22:25:50 -0500

    created react app, began creating my reducer component, and created my todo.js file.

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useReducer } from 'react';
+import './App.css';
+
+import { todoReducer, initialTodoState } from '../src/reducers/todoReducer'
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
